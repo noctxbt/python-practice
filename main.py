0fdc371b15f229945a49201943952b3396f60ab2
noctxbt <noctxbt@gmail.com>
2026-04-21 01:45:30 +0500

    main.py updated

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-a = 13
-b = 7
+a = 13.5
+b = 2.0
 
 total = a + b
 diff = a - b
 prod = a * b
-div1 = a / b
-div2 = a // b
-mod = a % b
+div = a / b
 exp = a ** b
 
 print(a, '+', b, '=', total)
 print(a, '-', b, '=', diff)
 print(a, '*', b, '=', prod)
-print(a, '/', b, '=', div1)
-print(a, '//', b, '=', div2)
-print(a, '%', b, '=', mod)
+print(a, '/', b, '=', div)
 print(a, '**', b, '=', exp)
