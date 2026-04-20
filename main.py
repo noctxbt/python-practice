81dd4a9c672fd3a5277d2f3006f05659a87d7e5b
noctxbt <noctxbt@gmail.com>
2026-04-21 01:54:53 +0500

    main.py updated

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-a = 13.5
-b = 2.0
+num1 = float(input())
+num2 = float(input())
 
-total = a + b
-diff = a - b
-prod = a * b
-div = a / b
-exp = a ** b
-
-print(a, '+', b, '=', total)
-print(a, '-', b, '=', diff)
-print(a, '*', b, '=', prod)
-print(a, '/', b, '=', div)
-print(a, '**', b, '=', exp)
+print(num1 * num2 * 0.5)
